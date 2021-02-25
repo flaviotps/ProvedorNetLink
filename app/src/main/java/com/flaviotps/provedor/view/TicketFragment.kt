1b74a9151b7cafbacbb77343d4cdb7210562526a
Flavio <flaviotps@gmail.com>
2021-02-24 22:43:01 -0300

    Add new API

--- app/src/main/java/com/flaviotps/provedor/view/TicketFragment.kt
+++ app/src/main/java/com/flaviotps/provedor/view/TicketFragment.kt
@@ -1,31 +1,54 @@
 package com.flaviotps.provedor.view
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import com.flaviotps.provedor.R
-import com.flaviotps.provedor.adapter.OnTicketListener
-import com.flaviotps.provedor.adapter.TicketAdapter
+import com.flaviotps.provedor.data.AppClient
 import com.flaviotps.provedor.data.AppTicket
+import com.flaviotps.provedor.data.TicketRequest
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class TicketFragment(private var tickets: MutableList<AppTicket>, private val onTicketListener: OnTicketListener) : Fragment() {
-    private lateinit var recyclerOpen: RecyclerView
+
+class TicketFragment(private val client: AppClient, private val ticket: AppTicket)  : BottomSheetDialogFragment() {
+
+    private lateinit var closeButton: ImageButton
+    private lateinit var code: Button
+    private lateinit var pdf: Button
+    private val viewModel: MainViewModel by sharedViewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_tickets, container, false)
+        return inflater.inflate(R.layout.fragment_ticket, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        recyclerOpen = view.findViewById(R.id.recyclerOpen)
-        context?.let {
-            TicketAdapter(tickets, it, onTicketListener).apply {
-                recyclerOpen.adapter = this
-                notifyDataSetChanged()
+        super.onViewCreated(view, savedInstanceState)
+        closeButton = view.findViewById(R.id.close)
+        pdf = view.findViewById(R.id.pdf)
+        code = view.findViewById(R.id.code)
+        code.setOnClickListener {
+            context?.let { context ->
+                val clipboard: ClipboardManager? = getSystemService(context, ClipboardManager::class.java)
+                val clip = ClipData.newPlainText("CÓDIGO BOLETO", ticket.code)
+                clipboard?.setPrimaryClip(clip)
+                Toast.makeText(context, "Código copiado", Toast.LENGTH_LONG).show()
             }
         }
-        super.onViewCreated(view, savedInstanceState)
+        pdf.setOnClickListener {
+            if (!client.login.isNullOrEmpty() && !client.password.isNullOrEmpty() && !ticket.id.isNullOrEmpty()) {
+                viewModel.getOpenTicket(TicketRequest(id = ticket.id, login = client.login, password = client.password))
+                dismiss()
+            }
+        }
+        closeButton.setOnClickListener { dismiss() }
+
     }
 }
